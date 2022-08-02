bb60c0c4a95cbc6b43a51b6ceb19cf12a2658bee
Vivek Chudasama <vvchudasamainfo@gmail.com>
2022-08-02 23:20:25 +0530

    added Multiply.cpp

---
+++ Multiply.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n1, n2, multiplication;
+    cout << "Enter two integers: ";
+    cin >> n1 >> n2;
+
+    multiplication = n1 * n2;
+
+    cout <<"Multiplication is here: "<< n1 << " * " << n2 << " = " << multiplication;
+
+    return 0;
+}
